aeb86f178f08dfc1bed79b8ce93d0ca55369f3da
davquipe <davidcondori99@gmail.com>
2017-11-15 16:40:53 -0500

    psbdhgsdfghsdrjhtsrtfgbsn.tjhñdsfthknldstyjesd

---
+++ ve_social/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\lp2-20172\django_social_login_tutorial\ve_social\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
