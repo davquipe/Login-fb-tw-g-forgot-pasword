cf3614938ac3e71f8226b12b9cab7b4cd7ad8da3
davquipe <davidcondori99@gmail.com>
2017-11-15 16:43:53 -0500

    ffffff

--- django_social_login_tutorial/settings.py
+++ django_social_login_tutorial/settings.py
@@ -129,7 +129,7 @@ LOGOUT_URL = reverse_lazy('logout')
 # EMAIL
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'davidcq99@gmail.com'
-EMAIL_HOST_PASSWORD = '123456789DAVIDquispe'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'davidcq99@gmail.com'
